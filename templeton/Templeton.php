0d54ad031c6dd19f179fa867bdc8c730fc28d301
wadkan <zsoltgaramszegi@gmail.com>
2020-12-01 18:02:06 +0100

    create constructor

---
+++ templeton/Templeton.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Templeton
+{
+    private string $folder_path;
+
+    public function __construct(string $folder_path)
+    {
+        $this->folder_path = $folder_path;
+    }
+
+
+}
