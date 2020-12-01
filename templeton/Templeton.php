bec965297922906afccfebded63e9526b261e6b3
wadkan <zsoltgaramszegi@gmail.com>
2020-12-01 18:35:41 +0100

    docs added inside. need yet: use phpDocumentator

--- templeton/Templeton.php
+++ templeton/Templeton.php
@@ -1,6 +1,10 @@
 <?php
 
 
+/**
+ * Class Templeton
+ * ........
+ */
 class Templeton
 {
     private string $folder_path;
@@ -10,6 +14,11 @@ class Templeton
         $this->folder_path = $folder_path;
     }
 
+    /**
+     * @param string $templateName This is the file name of the template to use.
+     * @param array $templateVariables This associtated array contains key value pairs, where key is the name of the variable, and the value is that will replace with.
+     * @return string The rendered template.
+     */
     public function run(string $templateName, array $templateVariables): string
     {
         $file = __DIR__ . '/' . $this->folder_path . '/' . $templateName;
