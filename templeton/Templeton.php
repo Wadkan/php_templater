4a4b6c1b03576a75bb3d154b5ba2035729e07cf5
wadkan <zsoltgaramszegi@gmail.com>
2020-12-01 18:16:00 +0100

    file reading works

--- templeton/Templeton.php
+++ templeton/Templeton.php
@@ -10,5 +10,16 @@ class Templeton
         $this->folder_path = $folder_path;
     }
 
+    public function run(string $templateName, array $templateVariables): string
+    {
+        $temp_full_path = __DIR__ . '/' . $this->folder_path . '/' . $templateName;
+        return readfile($temp_full_path);
+
+    }
 
 }
+
+$arr = ['name' => 'Wadkan', 'age' => 31];
+
+$a = new Templeton('template');
+echo $a->run('test.html', $arr);
